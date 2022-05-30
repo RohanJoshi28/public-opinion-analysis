9d40a14ed455e2587b31355652bee5e487bb753a
RohanJoshi28 <78099546+RohanJoshi28@users.noreply.github.com>
2022-05-30 16:48:44 -0400

    add text classification pipeline to server

--- app/server.py
+++ app/server.py
@@ -5,6 +5,18 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+model_saved=AutoModel.from_pretrained("RohanJoshi28/twitter_sentiment_analysisv1")
+model_name = "RohanJoshi28/twitter_sentiment_analysisv1"
+
+pipe = pipeline(model=model_name, tokenizer=tokenizer)
+
+@app.route("/classify")
+def classify_text():
+    if request.method=="POST":
+        text = request.form.get('text')
+        return str(pipe(text)[0]["label"][-1])
+
 @app.route('/')
 def home():
     return render_template("index.html")
